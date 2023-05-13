1cddde2531e1371113f4610b1ef52c81432715ff
r1l4r <rko.larsson@gmail.com>
2023-05-13 19:39:03 +0100

    feat: worldcoin

--- src/highlights/index.ts
+++ src/highlights/index.ts
@@ -3,11 +3,13 @@ import Uniswap from "./chain/Uniswap";
 import LensInterests from "./chain/LensInterests";
 import LensActivity from "./chain/LensActivity";
 import AirstackNFTs from "./chain/AirstackNFTs";
+import WorldCoin from "./chain/WorldCoin";
 
 const highlights = [
   firstTransaction(1),
   firstTransaction(10),
   firstTransaction(137),
+  WorldCoin,
   Uniswap,
   LensInterests,
   LensActivity,
